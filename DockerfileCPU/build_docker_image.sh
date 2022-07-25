e741495cd2c95b2df58c15c0b252e21db1870564
aoliverg <aoliverg@uoc.edu>
2022-07-25 18:21:47 +0200

    Add files via upload

--- DockerfileCPU/build_docker_image.sh
+++ DockerfileCPU/build_docker_image.sh
@@ -1,1 +1,2 @@
-sudo docker build . -t name_of_the_engine-20220530-spa-cat
+wget http://lpg.uoc.edu/marian-server-v1.11.0/marian-server-CPU
+sudo docker build . -t incytaep-transformer-20220606-cat-spa
